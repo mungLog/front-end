ef3a181c486a4e7e22fc61610e33c97c2c585520
정재은 <jjyjjy0528@naver.com>
2024-07-27 22:25:34 +0900

    재은 헤더 css, app.js 수정

--- src/App.js
+++ src/App.js
@@ -19,29 +19,27 @@ import FindPasswordPage from "./user/FindPasswordPage";
 
 function App() {
   return (
-    <div id="app">
+    <>
       <Header />
-      <div className="nnn">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          {/* <Route path="/shop" element={<Shop />} />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        {/* <Route path="/shop" element={<Shop />} />
         <Route path="/card" element={<Card />} />
         <Route path="/community" element={<CommunityList />} />
         <Route path="/community/write" element={<CommunityWrite />} />
         <Route path="/community/posts/:postId" element={<CommunityDetail />} /> */}
-          <Route path="/mypage" element={<Mypage />}>
-            <Route path="changeinfopage" element={<ChangeInfo />} />
-            <Route path="addfamilymember" element={<AddFamilyMember />} />
-            <Route path="purchasehistory" element={<PurchaseHistory />} />
-          </Route>
-          <Route path="/mbti" element={<MbtiPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<JoinPage />} />
-          <Route path="/findidpage" element={<FindIdPage />} />
-          <Route path="/findpasswordpage" element={<FindPasswordPage />} />
-        </Routes>
-      </div>
-    </div>
+        <Route path="/mypage" element={<Mypage />}>
+          <Route path="changeinfopage" element={<ChangeInfo />} />
+          <Route path="addfamilymember" element={<AddFamilyMember />} />
+          <Route path="purchasehistory" element={<PurchaseHistory />} />
+        </Route>
+        <Route path="/mbti" element={<MbtiPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<JoinPage />} />
+        <Route path="/findidpage" element={<FindIdPage />} />
+        <Route path="/findpasswordpage" element={<FindPasswordPage />} />
+      </Routes>
+    </>
   );
 }
 
